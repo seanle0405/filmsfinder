1176ac5534c82c56950f098b6a217d36901ed37b
Mitchell Chang <mitchellchang@Mitchells-MacBook-Air.local>
2019-06-29 11:53:40 -0700

    mongo db  movies

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 // requiring dependencies
 require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const app = express()
 const PORT = process.env.PORT;
 
 // Mongoose connection setup
-//
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'
-//
-// mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
-// mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
-//
-//
-// mongoose.connect('mongodb://localhost:27017/bookmarks', { useNewUrlParser: true })
-// mongoose.connection.once('open', ()=>{
-//     console.log('connected to mongoose...')
-// });
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'
+
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+
+mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...')
+});
 
 // Middlewares:
 app.use(express.json())
