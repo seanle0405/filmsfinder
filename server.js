7e94b7300f344eba1e4bfefa8efe2b6bbede526d
Victor Recabarren <victormrecabarren@gmail.com>
2019-06-29 12:20:48 -0400

    setup get route

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express')
 const app = express()
+const PORT = 3003
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('get / route')
+})
+
+app.listen(PORT, () => {
+  console.log('now listening on backend port: 3003 ');
+})
