169132d28ef68e9bd55bdb63899dc94069e7cb56
Victor Recabarren <victormrecabarren@gmail.com>
2019-06-30 23:24:30 -0400

    changed model so that each document will be a user, with a nested array of movie objects. added test routes to add movie to users

--- models/Movie.js
+++ models/Movie.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
 const movieSchema = mongoose.Schema({
-  title: {type: String, required: true, default: 'test'},
-  poster: {type: String, required: true, default: '.jpg'},
+  userID: {type: String, required: true},
+  name: {type: String},
+  movies: [
+      {
+      title: {type: String, required: true, default: 'test'},
+      poster: {type: String, required: true, default: '.jpg'}
+    }
+  ]
 })
 
 const Movie = mongoose.model('Movie', movieSchema)
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const MONGODB_URI = process.env.MONGODB_URI ||'mongodb://localhost:27017/filmfin
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
 
-mongoose.connect('mongodb://localhost:27017/filmfinder', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/filmfinder', { useNewUrlParser: true , useFindAndModify: false});
 mongoose.connection.once('open', ()=>{
     console.log('connected to mongoose...')
 });
