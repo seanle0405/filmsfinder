04da10e9d90c4b0bc0c0e2c141f3796ce81fa9d2
Kevin Honnoll <khonnoll@gmail.com>
2019-07-02 17:42:24 -0400

    added get and post routes to users controller

---
+++ controllers/sessions.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const bcrypt = require('bcrypt')
+const router = express.Router()
+const User = require('../models/User.js')
+
+router.get('/', (req, res) => {
+  req.session.
+})
--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,27 @@
 const express = require('express')
-const router = express.Router()
+const users = express.Router()
+const User = require('../models/User.js')
+const bcrypt = require('bcrypt')
 
-router.get('/new', (req, res) => {
-  res.send('users controller')
+users.get('/newuser', (req, res) => {
+  User.find({}, (error, foundUser) => {
+    if (error) {
+      res.status(400).json({error: error.message})
+    }
+      res.status(200).json(foundUser)
+  })
+})
+
+users.post('/', (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.gensaltSync(15))
+  User.create(req.body, (error, createdUser) => {
+    if (error) {
+      res.send('Account not created')
+    } else {
+      console.log(createdUser)
+      res.send(createdUser)
+    }
+  })
 })
 
 module.exports = router;
