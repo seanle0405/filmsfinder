36402277c2e65f26c2f033e99a9aeda339bbb231
Kevin Honnoll <khonnoll@gmail.com>
2019-07-02 23:25:07 -0400

    code for authenticated sessions partially built

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -3,8 +3,24 @@ const bcrypt = require('bcrypt')
 const session = express.Router()
 const User = require('../models/User.js')
 
-session.get('/', (req, res) => {
+session.post('/', (req, res) => {
+  User.findOne(
+    { username: req.body.username }, (error, foundUser) => {
+      if (error) res.status(400).json({error: error.message})
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        req.session.currentUser = foundUser
+        res.status(200).json(foundUser)
+      } else {
+        res.send('incorrect login info')
+      }
+    }
+  )
+})
 
+session.delete('/', (req, res) => {
+  req.session.destroy( () => {
+
+  })
 })
 
 module.exports = session
