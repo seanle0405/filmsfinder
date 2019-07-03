66cb3718887d1aa3a9e187a5beecb0d427f0914f
Sean Le <seanle0405@hotmail.com>
2019-07-03 16:58:10 -0700

    got search by title to work from Showtimes API

--- controllers/filmfinder.js
+++ controllers/filmfinder.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Movie = require('../models/Movie.js')
+const request = require("request")
 
 const testObject = {
   userID: 'testUserName',
@@ -61,6 +62,42 @@ router.get('/getUser/:id', (req, res) => {
 })
 
 
+
+//Showtimes API search by title
+
+router.get("/search/:title", (req, res) =>{
+  request.get("https://api.internationalshowtimes.com/v4/movies?apikey=dHNYEAlSVxOXC4Eqy6b8aufIXC7utYnu&search_field=original_title&include_outdated=true&all_fields=true&search_query=" +req.params.title, {json: true}, (err, response, body) =>{
+    if(err){
+      res.send(err)
+    }else{
+      let data = []
+      for(let i = 0; i < body.movies.length; i++){
+        const movie = {poster: ""}
+        movie.title = body.movies[i].title
+        if(body.movies[i].poster_image){
+          if(body.movies[i].poster_image.image_files){
+            let imageArr = body.movies[i].poster_image.image_files
+            movie.poster = imageArr[imageArr.length-1].url
+          }
+        }
+        if(body.movies[i].genres){
+          let genresData = body.movies[i].genres
+          let genresArr = []
+          for(let i = 0; i < genresData.length; i++){
+            genresArr.push(genresData[i].name)
+          }
+          movie.genres = genresArr
+        }
+        if(movie.title){
+          data.push(movie)
+        }       
+      }
+      res.send(data)
+    }
+    
+  })
+
+})
 
 
 router.post('/', (req, res) => {
