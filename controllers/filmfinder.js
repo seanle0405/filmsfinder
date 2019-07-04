fddd57478b7582e73256c08b99391e28b3321a05
Victor Recabarren <victormrecabarren@gmail.com>
2019-07-04 00:50:37 -0400

    working on getting user diary to myfilms component

--- controllers/filmfinder.js
+++ controllers/filmfinder.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const Movie = require('../models/Movie.js')
-const request = require("request")
 
 const testObject = {
   userID: 'testUserName',
@@ -39,11 +38,12 @@ router.get('/test', (req, res) => {
 
 //////test add movie route //////////////////////////////////
 
-router.post('/addMovie', (req, res) => {
-  const {username} = req.body
-  const {movie} = req.body
-  console.log('username:', username, 'movie: ', movie);
-  Movie.findOneAndUpdate({userID: username}, {$push: {movies: movie}}, {new: true}, (err, data) => {
+router.get('/addMovieToUser/:id', (req, res) => {
+
+
+  const newmovie = {title: '2001: A Space Odyssey', poster: '.jpg'}
+
+  Movie.findByIdAndUpdate(req.params.id, {$push: {movies: newmovie}}, {new: true}, (err, data) => {
     if (err) {
       res.status(400).json({error:err.message})
     }
@@ -55,51 +55,18 @@ router.post('/addMovie', (req, res) => {
 
 router.get('/getUser/:id', (req, res) => {
   Movie.find({userID: `${req.params.id}`}, (err, data) => {
-    if (err) res.status(400).json({error: err.message})
+    if (err) {
+      res.status(400).json({error: err.message})
+    }
+    console.log(data);
     res.status(200).json(data)
   })
 })
 
 
 
-//Showtimes API search by title
 
-router.get("/search/:title", (req, res) =>{
-  request.get("https://api.internationalshowtimes.com/v4/movies?apikey=dHNYEAlSVxOXC4Eqy6b8aufIXC7utYnu&search_field=original_title&include_outdated=true&all_fields=true&search_query=" +req.params.title, {json: true}, (err, response, body) =>{
-    if(err){
-      res.send(err)
-    }else{
-      let data = []
-      for(let i = 0; i < body.movies.length; i++){
-        const movie = {poster: ""}
-        movie.title = body.movies[i].title
-        if(body.movies[i].poster_image){
-          if(body.movies[i].poster_image.image_files){
-            let imageArr = body.movies[i].poster_image.image_files
-            movie.poster = imageArr[imageArr.length-1].url
-          }
-        }
-        if(body.movies[i].genres){
-          let genresData = body.movies[i].genres
-          let genresArr = []
-          for(let i = 0; i < genresData.length; i++){
-            genresArr.push(genresData[i].name)
-          }
-          movie.genres = genresArr
-        }
-        if(movie.title){
-          data.push(movie)
-        }
-      }
-      res.send(data)
-    }
-
-  })
-
-})
-
-
-router.post('/', (req, res) => {
+router.post('/userInfo', (req, res) => {
   res.send('post / route')
 });
 
